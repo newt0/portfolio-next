6290f24510f4d84984d1ceb73bc55af571139052
newt0 <py_lambda@yahoo.co.jp>
2021-02-03 19:20:04 +0900

    fix: cleaner urls with withRouter

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,10 @@
 import Layout from "../components/Layout";
+import { withRouter } from "next/router";
 
-const Post = ({ url }) => (
-  <Layout title={url.query.title}>
+const Post = ({ router }) => (
+  <Layout title={router.query.title}>
     <p>ttttttttttttttttttttttttttttttttttttttttttttttttt</p>
   </Layout>
 );
 
-export default Post;
+export default withRouter(Post);
