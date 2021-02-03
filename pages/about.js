6124b354fa3148760116d7f282dcf572c1a38148
newt0 <py_lambda@yahoo.co.jp>
2021-02-03 17:30:52 +0900

    add: fetch data with componentDidMount

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,29 @@
 import Layout from "../components/Layout";
-
-export default () => {
-  return (
-    <Layout title="about">
-      <p>React Developer</p>
-      <img src="/static/reactlogo.png" alt="" height="200px" />
-    </Layout>
-  );
-};
+import Link from "next/link";
+import { Component } from "react";
+export default class About extends Component {
+  state = {
+    user: null,
+  };
+  componentDidMount() {
+    fetch("https://api.github.com/users/newt0")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          user: data,
+        });
+      });
+  }
+  render() {
+    return (
+      <Layout title="about">
+        {JSON.stringify(this.state.user)}
+        <Link href="/">
+          <a>Go to Home</a>
+        </Link>
+        <p>React Developer</p>
+        // <img src="/static/reactlogo.png" alt="" height="200px" />
+      </Layout>
+    );
+  }
+}
